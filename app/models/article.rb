14ad12af8bde68fd85880a76c2246251c60cf1d8
abdo <abdelkhalekalashker@gmail.com>
2023-03-02 02:41:37 +0200

    SEO optmization using friendly_id gem with the URL history

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  extend FriendlyId
+
     validates :title, presence:true,length: {minimum:3, maximum:45}
     validates :body, presence:true , length: {minimum:1, maximu:300}
     belongs_to :user
@@ -10,7 +12,11 @@ class Article < ApplicationRecord
     has_noticed_notifications model_name: "Notification"
     has_many :notifications, through: :user
 
-    def self.ransackable_associations(auth_object = nil)
-        ["title","body","user_email","user_name"]
-      end
+    friendly_id :title, use: %i[slugged history finders] # history to save old urls and redirect to the new url and finder here instead to change all Post.friendly.find(params[:id]) and just become Post.find(params[:id])
+
+    def should_generate_new_friendly_id?
+      title_changed? || slug.blank?
+    end
+
+
 end
