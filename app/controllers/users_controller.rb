ab26356ddae79d9533f592824a4f473e8c4cdc00
abdo <abdelkhalekalashker@gmail.com>
2023-03-06 17:09:33 +0200

    edit profile page and add avatar to user to display in profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   before_action :set_user
-  
+
   def profile
       @user.update!(views: @user.views + 1)
+      @articles =@user.articles.includes(:rich_text_body).order(created_at: :desc)
+
+      @total_views = 0
+      @articles.each do |article|
+        @total_views += article.views
+      end
   end
 
   private
